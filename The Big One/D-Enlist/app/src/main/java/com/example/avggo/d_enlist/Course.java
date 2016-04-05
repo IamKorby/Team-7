a25225a13969ed2e7294ef40f269ba464cd76c66
Kevin Chan <kevin98gray@gmail.com>
2016-04-05 15:08:14 +0800

    Create database

--- The Big One/D-Enlist/app/src/main/java/com/example/avggo/d_enlist/Course.java
+++ The Big One/D-Enlist/app/src/main/java/com/example/avggo/d_enlist/Course.java
@@ -5,6 +5,18 @@ package com.example.avggo.d_enlist;
  */
 public class Course {
 
+    public static final String TABLE_NAME             = "course";
+    public static final String COLUMN_CLASS_NUMBER    = "classNumber";
+    public static final String COLUMN_COURSE_NAME     = "courseName";
+    public static final String COLUMN_SECTION         = "section";
+    public static final String COLUMN_DAYS            = "days";
+    public static final String COLUMN_START_TIME      = "startTime";
+    public static final String COLUMN_END_TIME        = "endTime";
+    public static final String COLUMN_ROOM            = "room";
+    public static final String COLUMN_PROFESSOR       = "professor";
+    public static final String COLUMN_ENROLL_CAPACITY = "enrollCapacity";
+    public static final String COLUMN_ENROLLED        = "enrolled";
+
     String classNumber;
     String courseName;
     String section;
@@ -12,12 +24,123 @@ public class Course {
     String startTime;
     String endTime;
     String room;
+    String professor;
     int enrollCapacity;
     int enrolled;
 
     public Course() {
-
+        this.classNumber = "";
+        this.courseName = "";
+        this.section = "";
+        this.startTime = "";
+        this.days = "";
+        this.endTime = "";
+        this.room = "";
+        setEnrollCapacity(0);
+        this.professor = "";
+        setEnrolled(0);
     }
 
+    public Course(String classNumber, String courseName, String section, String startTime,
+                  String days, String endTime, String room, int enrollCapacity, String professor,
+                  int enrolled) {
+        this.classNumber = classNumber;
+        this.courseName = courseName;
+        this.section = section;
+        this.startTime = startTime;
+        this.days = days;
+        this.endTime = endTime;
+        this.room = room;
+        //this.enrollCapacity = enrollCapacity;
+        setEnrollCapacity(enrollCapacity);
+        this.professor = professor;
+        //this.enrolled = enrolled;
+        setEnrolled(enrolled);
+    }
 
+    public String getClassNumber() {
+        return classNumber;
+    }
+
+    public void setClassNumber(String classNumber) {
+        this.classNumber = classNumber;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getDays() {
+        return days;
+    }
+
+    public void setDays(String days) {
+        this.days = days;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public String getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(String professor) {
+        this.professor = professor;
+    }
+
+    public int getEnrolled() {
+        return enrolled;
+    }
+
+    public void setEnrolled(int enrolled) {
+        if (enrolled == 0 && enrolled > this.enrollCapacity)
+            this.enrolled = 0;
+
+        this.enrolled = enrolled;
+    }
+
+    public int getEnrollCapacity() {
+        return enrollCapacity;
+    }
+
+    public void setEnrollCapacity(int enrollCapacity) {
+        if (enrollCapacity < 0)
+            this.enrollCapacity = 0;
+
+        this.enrollCapacity = enrollCapacity;
+    }
 }
--- The Big One/D-Enlist/app/src/main/java/com/example/avggo/d_enlist/DatabaseOpenHelper.java
+++ The Big One/D-Enlist/app/src/main/java/com/example/avggo/d_enlist/DatabaseOpenHelper.java
@@ -1,6 +1,7 @@
 package com.example.avggo.d_enlist;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -17,7 +18,62 @@ public class DatabaseOpenHelper  extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String sql = "CREATE TABLE ";
+        String sql = "CREATE TABLE " + Course.TABLE_NAME + " (" +
+                Course.COLUMN_CLASS_NUMBER + " INTEGER PRIMARY KEY, " +
+                Course.COLUMN_COURSE_NAME + " TEXT, " +
+                Course.COLUMN_SECTION + " TEXT, " +
+                Course.COLUMN_DAYS + " TEXT, " +
+                Course.COLUMN_START_TIME + " TEXT, " +
+                Course.COLUMN_END_TIME + " TEXT, " +
+                Course.COLUMN_ROOM + " TEXT, " +
+                Course.COLUMN_PROFESSOR + " TEXT, " +
+                Course.COLUMN_ENROLL_CAPACITY + " TEXT, " +
+                Course.COLUMN_ENROLLED + " TEXT);";
+
+        db.execSQL(sql);
+    }
+
+    public Course queryCourse(String classNumber) {
+        SQLiteDatabase db = getReadableDatabase();
+
+        Cursor c = db.query(Course.TABLE_NAME, null, Course.COLUMN_CLASS_NUMBER + " = ?", new String[]{classNumber}, null, null, null);
+
+        Course newCourse;
+
+        if (c.moveToFirst()) {
+            newCourse = new Course();
+            newCourse.setClassNumber(c.getString(c.getColumnIndex(Course.COLUMN_CLASS_NUMBER)));
+            newCourse.setCourseName(c.getString(c.getColumnIndex(Course.COLUMN_COURSE_NAME)));
+            newCourse.setSection(c.getString(c.getColumnIndex(Course.COLUMN_SECTION)));
+            newCourse.setDays(c.getString(c.getColumnIndex(Course.COLUMN_DAYS)));
+            newCourse.setStartTime(c.getString(c.getColumnIndex(Course.COLUMN_START_TIME)));
+            newCourse.setEndTime(c.getString(c.getColumnIndex(Course.COLUMN_END_TIME)));
+            newCourse.setRoom(c.getString(c.getColumnIndex(Course.COLUMN_ROOM)));
+            newCourse.setProfessor(c.getString(c.getColumnIndex(Course.COLUMN_PROFESSOR)));
+            newCourse.setEnrollCapacity(c.getInt(c.getColumnIndex(Course.COLUMN_ENROLL_CAPACITY)));
+            newCourse.setEnrolled(c.getInt(c.getColumnIndex(Course.COLUMN_ENROLLED)));
+        }
+        else {
+            newCourse = null;
+        }
+
+        return newCourse;
+    }
+
+    public Cursor queryCourseName(String courseName) {
+        SQLiteDatabase db = getReadableDatabase();
+
+        Cursor c = db.query(Course.TABLE_NAME, null, Course.COLUMN_COURSE_NAME + " = ?", new String[]{courseName}, null, null, null);
+
+        return c;
+    }
+
+    public Cursor queryAllCourses() {
+        SQLiteDatabase db = getReadableDatabase();
+
+        Cursor c = db.query(Course.TABLE_NAME, null, null, null, null, null, null);
+
+        return c;
     }
 
     @Override
